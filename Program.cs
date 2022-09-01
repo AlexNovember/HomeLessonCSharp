60a55fc02f6cb7d43791599e02cac82080a21802
Alex <tanko77@rambler.ru>
2022-09-01 20:16:33 +0300

    1задача1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿
-    internal class Program
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-    Console.WriteLine("Введите первое число: ");
+        Console.WriteLine("Введите первое число: ");
         int number1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число: ");
+        Console.WriteLine("Введите второе число: ");
         int number2 = Convert.ToInt32(Console.ReadLine());
-        
+
         if (number1 > number2)
-            {
-    Console.WriteLine("Первое число больше второго");
-            }
-            else if (number1 < number2)
-            {
-     Console.WriteLine("Первое число меньше второго");
-            }
-            else
-            {
-     Console.WriteLine("Ччисла равны");
-            }
+        {
+            Console.WriteLine("Первое число больше второго");
+        }
+        else if (number1 < number2)
+        {
+            Console.WriteLine("Первое число меньше второго");
+        }
+        else
+        {
+            Console.WriteLine("Ччисла равны");
         }
     }
+}
