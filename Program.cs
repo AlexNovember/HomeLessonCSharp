3548656795c9ff5785edd5f92b146417ecd5eed7
Alex <tanko77@rambler.ru>
2022-09-01 20:13:17 +0300

    1задача

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿            Console.WriteLine("Введите первое число: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
- 
-            if(num1 > num2)
+﻿
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+    Console.WriteLine("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        
+        if (number1 > number2)
             {
-                Console.WriteLine("Первое число больше второго");
+    Console.WriteLine("Первое число больше второго");
             }
-            else if (num1 < num2)
+            else if (number1 < number2)
             {
-                Console.WriteLine("Первое число меньше второго");
+     Console.WriteLine("Первое число меньше второго");
             }
             else
             {
-                Console.WriteLine("Оба числа равны");
+     Console.WriteLine("Ччисла равны");
             }
-            
+        }
+    }
