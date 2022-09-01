c6294eb98a1fb4a849b72df0915276a433ffbd9e
Alex <tanko77@rambler.ru>
2022-09-01 20:34:02 +0300

    2zadacha

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Введите первое число: ");
-        int number1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите второе число: ");
-        int number2 = Convert.ToInt32(Console.ReadLine());
+﻿// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Введите первое число: ");
+//         int number1 = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Введите второе число: ");
+//         int number2 = Convert.ToInt32(Console.ReadLine());
 
-        if (number1 > number2)
-        {
-            Console.WriteLine("Первое число больше второго");
-        }
-        else if (number1 < number2)
-        {
-            Console.WriteLine("Первое число меньше второго");
-        }
-        else
-        {
-            Console.WriteLine("Ччисла равны");
-        }
-    }
-}
+//         if (number1 > number2)
+//         {
+//             Console.WriteLine("Первое число больше второго");
+//         }
+//         else if (number1 < number2)
+//         {
+//             Console.WriteLine("Первое число меньше второго");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Числа равны");
+//         }
+//     }
+// }
+
+
+
+Console.Write("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+    int max=number1;
+
+if (number2>max)max=number2;
+if (number3>max)max=number3;
+Console.WriteLine("Максимальное число " +max);
