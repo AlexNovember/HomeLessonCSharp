4cec5e39f16838c1e1d349621c3effc26b51d038
Alex <tanko77@rambler.ru>
2022-09-01 20:40:45 +0300

    3zadacha

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 // {
 //     private static void Main(string[] args)
 //     {
-//         Console.WriteLine("Введите первое число: ");
+//         Console.Write("Введите первое число: ");
 //         int number1 = Convert.ToInt32(Console.ReadLine());
-//         Console.WriteLine("Введите второе число: ");
+//         Console.Write("Введите второе число: ");
 //         int number2 = Convert.ToInt32(Console.ReadLine());
 
 //         if (number1 > number2)
@@ -24,14 +24,17 @@
 
 
 
-Console.Write("Введите первое число: ");
-        int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-        int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-        int number3 = Convert.ToInt32(Console.ReadLine());
-    int max=number1;
+// Console.Write("Введите первое число: ");
+//         int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+//         int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+//         int number3 = Convert.ToInt32(Console.ReadLine());
+//     int max=number1;
 
-if (number2>max)max=number2;
-if (number3>max)max=number3;
-Console.WriteLine("Максимальное число " +max);
+// if (number2>max)max=number2;
+// if (number3>max)max=number3;
+// Console.WriteLine("Максимальное число " +max);
+
+
+
